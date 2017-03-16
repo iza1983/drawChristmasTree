c51c5b9472a60b3b7dc0f2d387de5f9d56733cea
iza1983 <imalnowicz@gmail.com>
2017-03-16 13:04:15 +0100

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,7 @@
 function drawChristmasTree(n) {
   for(var i = 0; i < n; i++) {
     var star = '';
-        for(var k = 0; k < n - i - 1; k ++) {
+        for(var k = 0; k < n - i - 1; k++) {
         star += ' '
     }    
     for(var j = 0; j < (i * 2  + 1); j++) {
